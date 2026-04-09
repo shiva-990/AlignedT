8fbbb0cfd5babb3964f1b127cc315dde4b4d8de4
shiva-990 <shivachandra151@gmail.com>
2026-04-09 14:50:28 +0530

    Delete Practiceset directory

--- Practiceset/binarytodecimal8.py
+++
@@ -1,7 +0,0 @@
-binary=int(input("Enter a binary number: "))
-decimal=0
-power=0
-for i in reversed(str(binary)):
-    decimal+=int(i)*(2**power)
-    power+=1
-print("Decimal equivalent: ",decimal)
